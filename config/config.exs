71677b78b49258872475c29cbcc858933e4d9e13
Pedro Morte Rolo <pedrorolo@gmail.com>
2026-08-21 17:27:59 +0100

    phx.auth.gen

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :unclickbaiter, :scopes,
+  user: [
+    default: true,
+    module: Unclickbaiter.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Unclickbaiter.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :unclickbaiter,
   env: config_env(),
   ecto_repos: [Unclickbaiter.Repo],
--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Unclickbaiter.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.8.9"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.13"},
