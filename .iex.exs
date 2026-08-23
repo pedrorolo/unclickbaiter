39c3b236c3a64b5331875b07b608892077afb2f0
Pedro Morte Rolo <pedrorolo@gmail.com>
2026-08-23 20:16:46 +0100

    .iex.exs

---
+++ .iex.exs
@@ -0,0 +1,7 @@
+# Import functions/macros for direct use without prefixes
+import Ecto.Query
+
+# Alias individual schema and context modules
+alias Unclickbaiter.{Repo, Accounts, Previews, PreviewMetadata}
+alias Accounts.User
+alias Previews.Preview
